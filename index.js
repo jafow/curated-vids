36c3346b91a8336a294d7a497d441c2dabfaf26b
jafow <jared.a.fowler@gmail.com>
2017-11-09 14:25:14 -0800

    Fetch videos and render; adds styles

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-/* global YT */
+/* global YT, fetch, Headers */
 const html = require('choo/html')
 const choo = require('choo')
+const css = require('sheetify')
 const app = choo()
-
+const config = require('./config.json')
+const YT_API_TOKEN = config.YT_API_TOKEN
+const YT_PLAYLIST_ID = config.YT_PLAYLIST_ID
+css('tachyons')
+// const header = css('./assets/styles/headers.css')
+const main = css('./assets/styles/main.css')
+const pf = css`
+  :host {
+    color: yellow;
+  }`
+const pf2 = css`.friend { background: red; }`
+app.use(require('choo-devtools')())
 app.use(initialState)
+app.use(getVideos)
 app.route('/', mainView)
 app.mount('body')
 
 function mainView (state, emit) {
-  function videoClick () {
-    let vId = Math.floor(Math.random() * 100)
-    console.log('video click', vId)
-    emit('videoClick', vId)
-  }
-
   var player
-  window.onYouTubeIframeAPIReady = function onYouTubeIframeAPIReady() {
-    console.log('iddframe api read');
+  if (!state.items) state.items = []
+  window.onYouTubeIframeAPIReady = function onYouTubeIframeAPIReady () {
+    console.log('iddframe api read')
     player = new YT.Player('player', {
       height: '390',
       width: '640',
@@ -26,24 +34,30 @@ function mainView (state, emit) {
         'onStateChange': onPlayerStateChange
       }
     })
-    console.log('player is: ', player)
   }
 
+  function playOn () {
+    player.playVideo()
+  }
   function onPlayerReady (event) {
-    console.log('ready freddie')
     event.target.playVideo()
   }
   function onPlayerStateChange () {
     console.log('player stae change')
   }
   return html`
-    <body>
-      <h1>hello world</h1>
-      <h2>${state.message}</h2>
+    <body class=${pf2}>
+      <h1 class=${pf2}>hello world</h1>
+      <h2 class=${pf}>${state.message}</h2>
       <h3>${state.currentVideo}</h3>
       <ul>
-        <li>vid 1</li>
-        <li>vid 2</li>
+  ${state.items.map(function (vid) {
+    return html`
+        <li class=${main}>
+          <p>${vid.title}</p>
+          <img src=${vid.thumbnail} data-vidId=${vid.videoId}/>
+        </li>`
+  })}
       </ul>
 
     <script async src="https://www.youtube.com/iframe_api"></script>
@@ -57,4 +71,45 @@ function initialState (state, emitter) {
     state.currentVideo = vidId
     emitter.emit('render')
   })
+}
+
+function getVideos (state, emitter) {
+  var headers = new Headers()
+  var maxResults = 25
+  var playlistId = YT_PLAYLIST_ID
+  var part = 'snippet%2CcontentDetails'
+  var key = YT_API_TOKEN
+
+  var ytURL = 'https://www.googleapis.com/youtube/v3/playlistItems'
+  var ytFetchURL = `${ytURL}?key=${key}&maxResults=${maxResults}&playlistId=${playlistId}&part=${part}`
+  var fetchInit = {
+    method: 'GET',
+    headers: headers,
+    mode: 'cors',
+    cache: 'default'
+  }
+
+  emitter.on('DOMContentLoaded', function () {
+    state.items = []
+    fetch(ytFetchURL, fetchInit).then((response) => {
+      if (response.ok) {
+        let contentType = response.headers.get('content-type')
+        if (contentType && /application\/json/.test(contentType)) {
+          return response.json()
+        }
+      }
+    }).then((body) => {
+      let b = body
+      state.items = b.items.map(formatItem)
+      emitter.emit('render')
+    })
+  })
+}
+
+function formatItem (item) {
+  return {
+    title: item.snippet.title,
+    thumbnail: item.snippet.thumbnails.default.url,
+    videoId: item.snippet.resourceId.videoId
+  }
 }
